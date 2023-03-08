67fb6fef8e694e71e163a13b8eb28abb2ceaeb2a
vivekgoswami934 <vivekgoswamidbng@gmail.com>
2023-03-08 21:46:26 +0530

    query and resolver with variable completed

---
+++ server/fakedb.js
@@ -0,0 +1,33 @@
+
+export const users = [
+    {
+        id : "23131",
+        firstName : "Vivek",
+        lastName : "Goswami",
+        email : "vivekgoswami@gmail.com",
+        passowrd : "12345"
+    },
+    {
+        id : "45378",
+        firstName : "Vikas",
+        lastName : "Goswami",
+        email : "vikasgoswami@gmail.com",
+        passowrd : "12345"
+    },
+]
+
+
+export const quotes = [
+    {
+        name : "I turn coffee into code",
+        by : "23131"
+    },
+    {
+        name : "I am another code",
+        by : "23131"
+    },
+    {
+        name : "If code is work don't touch it 45378",
+        by : "45378"
+    },
+]
--- server/index.js
+++ server/index.js
@@ -1,19 +1,42 @@
 import { ApolloServer, gql } from "apollo-server";
-
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
+import {quotes,users} from  "./fakedb.js"
 
 //tagged template literal
 const typeDefs = gql`
    type Query{
-     greet : String
+     users : [User]
+     user(id:ID!):User                      
+     quotes : [Quote]
+     indvidualQuote(by : ID!) : [Quote]
+   }
+
+   type User{
+    id : ID!
+    firstName : String
+    lastName : String
+    email : String
+    password : String
+    quotes : [Quote]
+   }
+
+   type Quote{
+    name : String 
+    by : ID!
    }
 `
 
+// line 9  -->  user(id:ID!):User for individual
+
 const resolvers = {
     Query: {
-        greet: () => {
-            return "Hello World"
-        }
+        users : () => users,
+        user : (_,args) => users.find(user => user.id === args.id),              
+        quotes : () => quotes,
+        indvidualQuote :  (_,{by}) => quotes.filter(quote => quote.by === by), 
+    },
+    User : {
+        quotes : (ur) => quotes.filter(quote => quote.by == ur.id)   // ur ---> user
     }
 }
 
@@ -23,8 +46,10 @@ const server = new ApolloServer({
     plugins : [
         ApolloServerPluginLandingPageGraphQLPlayground()
     ]
-    
 })
+
+
+
 
 
 server.listen().then(({ url }) => {
